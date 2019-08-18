fb86ff8425530b2d803ef424ce44565248669d1a
Chris Lacey <ChrisLacey89@gmail.com>
2019-08-17 22:01:56 -0400

    added get route

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+const todos = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false
+  }
+];
+
+app.get('/api/todos', (req, res) => {
+  res.json(todos);
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'Public')));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
