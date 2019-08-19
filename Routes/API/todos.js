fd5cc45522a1e46d113a3fc4a1c265b8c5e605bd
Chris Lacey <ChrisLacey89@gmail.com>
2019-08-19 13:20:43 -0400

    push new todo to end

--- Routes/API/todos.js
+++ Routes/API/todos.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const todos = require('../../todos');
 
+const uuid = require('uuid');
+
 // Gets all todos
 router.get('/', (req, res) => {
   res.json(todos);
@@ -22,7 +24,18 @@ router.get('/:id', (req, res) => {
 
 // Create todo
 router.post('/', (req, res) => {
-  res.send(req.body);
+  const newTodo = {
+    userId: 1,
+    id: uuid.v4(),
+    title: req.body.title,
+    completed: false
+  };
+  if (!newTodo.title) {
+    return res.status(400).json({ msg: 'Please add a todo before submitting' });
+  }
+
+  todos.push(newTodo);
+  res.json(todos);
 });
 
 module.exports = router;
