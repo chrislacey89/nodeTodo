81dd70b13818934c532796ad3c9a074feebb2079
Chris Lacey <ChrisLacey89@gmail.com>
2019-08-18 21:01:44 -0400

    get single todo

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 const todos = require('./todos');
+const logger = require('./Middleware/logger');
 
 const app = express();
+
+// Init middleware
+// app.use(logger);
 
 // Gets all todos
 app.get('/api/todos', (req, res) => {
   res.json(todos);
+});
+
+// Get single todo
+app.get('/api/todos/:id', (req, res) => {
+  res.json(todos.filter(todo => todo.id === parseInt(req.params.id)));
 });
 
 // Set static folder
