be2d2ed961e13387163c575d0efe0bc83f2ed5ff
Chris Lacey <ChrisLacey89@gmail.com>
2019-08-19 13:10:31 -0400

    post & body parser middleware

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 // Init middleware
 // app.use(logger);
 
+// Body Parser Middleware
+//par
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'Public')));
 
