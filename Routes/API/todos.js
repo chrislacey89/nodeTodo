9fbf40cfdd43747c485f501e3480afb20586c2ac
Chris Lacey <ChrisLacey89@gmail.com>
2019-08-20 13:36:50 -0400

    added feed controller

--- Routes/API/todos.js
+++ Routes/API/todos.js
@@ -1,78 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const todos = require('../../todos');
 
-const uuid = require('uuid');
+const feedController = require('../../controllers/feed');
 
 // Gets all todos
-router.get('/', (req, res) => {
-  res.json(todos);
-});
+router.get('/', feedController.getTodos);
 
 // Get single todo
-router.get('/:id', (req, res) => {
-  const found = todos.some(todo => todo.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(todos.filter(todo => todo.id === parseInt(req.params.id)));
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No todo item with the id of ${req.params.id}` });
-  }
-});
+router.get('/:id', feedController.getSingleTodo);
 
 // Create todo
-router.post('/', (req, res) => {
-  const newTodo = {
-    userId: 1,
-    id: uuid.v4(),
-    title: req.body.title,
-    completed: false
-  };
-  if (!newTodo.title) {
-    return res.status(400).json({ msg: 'Please add a todo before submitting' });
-  }
-
-  todos.push(newTodo);
-  res.json(todos);
-});
+router.post('/', feedController.createTodo);
 
 // Update todo item
-router.put('/:id', (req, res) => {
-  const found = todos.some(todo => todo.id === parseInt(req.params.id));
-
-  if (found) {
-    const updateTodo = req.body;
-    todos.forEach(todo => {
-      if (todo.id === parseInt(req.params.id)) {
-        // if title or completed are updated and sent with request. If yes, we set to new title or completed
-        todo.title = updateTodo ? updateTodo.title : todo.title;
-        todo.completed = updateTodo ? updateTodo.completed : todo.completed;
-
-        res.json({ msg: 'Todo was updated', todo: todo });
-      }
-    });
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No todo item with the id of ${req.params.id}` });
-  }
-});
+router.put('/:id', feedController.updateTodo);
 
 // Delete Member
-router.delete('/:id', (req, res) => {
-  const found = todos.some(todo => todo.id === parseInt(req.params.id));
+router.delete('/:id', feedController.deleteTodo);
 
-  if (found) {
-    res.json({
-      msg: 'Todo item deleted',
-      todos: todos.filter(todo => todo.id !== parseInt(req.params.id))
-    });
-  } else {
-    res
-      .status(400)
-      .json({ msg: `No todo item with the id of ${req.params.id}` });
-  }
-});
 module.exports = router;
---
+++ controllers/feed.js
@@ -0,0 +1,74 @@
+const todos = require('../todos');
+const uuid = require('uuid');
+
+// Gets all todos
+exports.getTodos = (req, res) => {
+  res.json(todos);
+};
+
+// Get single todo
+exports.getSingleTodo = (req, res) => {
+  const found = todos.some(todo => todo.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(todos.filter(todo => todo.id === parseInt(req.params.id)));
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No todo item with the id of ${req.params.id}` });
+  }
+};
+
+// Create todo
+exports.createTodo = (req, res) => {
+  const newTodo = {
+    userId: 1,
+    id: uuid.v4(),
+    title: req.body.title,
+    completed: false
+  };
+  if (!newTodo.title) {
+    return res.status(400).json({ msg: 'Please add a todo before submitting' });
+  }
+
+  todos.push(newTodo);
+  res.json(todos);
+};
+
+// Update todo item
+exports.updateTodo = (req, res) => {
+  const found = todos.some(todo => todo.id === parseInt(req.params.id));
+
+  if (found) {
+    const updateTodo = req.body;
+    todos.forEach(todo => {
+      if (todo.id === parseInt(req.params.id)) {
+        // if title or completed are updated and sent with request. If yes, we set to new title or completed
+        todo.title = updateTodo ? updateTodo.title : todo.title;
+        todo.completed = updateTodo ? updateTodo.completed : todo.completed;
+
+        res.json({ msg: 'Todo was updated', todo: todo });
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No todo item with the id of ${req.params.id}` });
+  }
+};
+
+// Delete Member
+exports.deleteTodo = (req, res) => {
+  const found = todos.some(todo => todo.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: 'Todo item deleted',
+      todos: todos.filter(todo => todo.id !== parseInt(req.params.id))
+    });
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No todo item with the id of ${req.params.id}` });
+  }
+};
